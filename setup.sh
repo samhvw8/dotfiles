8342995084eddce569ec3713b2f6dac5595749dd
sam hoang <samhv.ict@gmail.com>
2021-09-08 16:36:05 +0700

    update

--- setup.sh
+++ setup.sh
@@ -3,26 +3,25 @@ then
     echo "Macos"
 else
     echo "Linux"
-    sudo apt-get install --yes zsh unzip apt-transport-https ca-certificates curl software-properties-common build-essential terminator 
+    sudo apt-get install --yes zsh unzip apt-transport-https ca-certificates curl software-properties-common build-essential terminator  && \
+    git clone https://github.com/kazhala/dotbare.git ~/.dotbare  && \
 
-    git clone https://github.com/kazhala/dotbare.git ~/.dotbare
+    export PATH=$PATH:$HOME/.dotbare && \
 
-    export PATH=$PATH:$HOME/.dotbare
+    echo 'alias dotbare="$HOME/.dotbare/dotbare"' | tee -a ~/.profile && \
 
-    echo 'alias dotbare="$HOME/.dotbare/dotbare"' | tee -a ~/.profile
+    dotbare finit -u  https://github.com/samhvw8/dotfiles.git && \
 
-    dotbare finit -u  https://github.com/samhvw8/dotfiles.git
+    sudo dbus-uuidgen --ensure && \
 
-    sudo dbus-uuidgen --ensure
+    wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local && \
 
-    wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+    echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a ~/.profile && \
 
-    echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a ~/.profile
+    source ~/.profile && \
 
-    source ~/.profile
+    curl -s "https://get.sdkman.io" | bash && \
 
-    curl -s "https://get.sdkman.io" | bash
-
-    chsh -s $(which zsh)
+    chsh -s $(which zsh) 
 
 fi
