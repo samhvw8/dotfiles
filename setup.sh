27e3de1e55b388943a60873c1ae5ddc98315a97e
sam hoang <samhv.ict@gmail.com>
2021-09-23 10:31:35 +0700

    update

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ else
 
     chmod +x "$HOME/.local/bin/kubectl" && \
 
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
+    git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && \
 
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash && \
 
