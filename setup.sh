499cec70da34336f5bf6a4f3d15713135eead9c9
sam hoang <samhv.ict@gmail.com>
2021-11-12 18:47:22 +0700

    fix git command

--- setup.sh
+++ setup.sh
@@ -106,6 +106,8 @@ else
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
 
     bash ~/miniconda.sh -b -p $HOME/miniconda3 && \
+
+    cargo install --locked git-branchless && \
     
     sudo chsh -s $(which zsh) $USERNAME
 fi
