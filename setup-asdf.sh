bfb46fc36fd58e19d0027743e498f2c4fb3aedcc
Sam Hoang <sam.hoang@mservice.com.vn>
2022-05-05 11:43:30 +0700

    fix asdf exec

--- setup-asdf.sh
+++ setup-asdf.sh
@@ -12,12 +12,12 @@ cd ~/workspace
 
 git clone https://github.com/danhper/asdf-exec.git
 
-cd ~/asdf-exec
+cd asdf-exec
 
 go build
 
 cp asdf-exec $HOME/.zi/plugins/asdf-vm---asdf/bin/private
 
-sed -i.bak -e 's|exec $(asdf_dir)/bin/asdf exec|exec $(asdf_dir)/bin/private/asdf-exec|' $HOME/.zi/plugins/asdf-vm---asdf/lib/commands/reshim.bash \ 
-&& rm ~/.asdf/shims/* \
-&& asdf reshim
+sed -i.bak -e 's|exec $(asdf_dir)/bin/asdf exec|exec $(asdf_dir)/bin/private/asdf-exec|' $HOME/.zi/plugins/asdf-vm---asdf/lib/commands/reshim.bash && \
+rm ~/.asdf/shims/* && \
+asdf reshim
