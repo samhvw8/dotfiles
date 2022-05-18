8056233b03425944db92a18974ea41166a18e937
sam hoang <samhv.ict@gmail.com>
2022-05-18 14:26:28 +0700

    update

--- setup.sh
+++ setup.sh
@@ -142,7 +142,6 @@ if [[ $(uname) == "Darwin" ]]; then
 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval $(/opt/homebrew/bin/brew shellenv)
-    brew bundle install
 else
     echo "Linux"
         sudo apt update &&
