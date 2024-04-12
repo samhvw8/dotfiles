eba322f4cc2c715a947d555fc64e5aafc2d095a5
sam hoang <samhv.ict@gmail.com>
2024-04-12 17:48:19 +0700

    update

--- setup-minimal.sh
+++ setup-minimal.sh
@@ -15,7 +15,6 @@ setup_folder() {
     export XDG_STATE_HOME="${HOME}/.local/state"
     export XDG_RUNTIME_DIR="${HOME}/.local/run"
 
-    export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
     export ZSH_DATA_DIR="${XDG_DATA_HOME}/zsh"
     export ZSH_CACHE_DIR="${XDG_CACHE_HOME}/zsh"
     export ZSH_COMPDUMP="${ZSH_CACHE_DIR}/zcompdump"
